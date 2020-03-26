f3f480f50bd7f7d0920cb72a769865579a8ffb94
Andres Urdaneta <aeum3893@gmail.com>
2020-03-26 17:30:02 -0400

    navbar and centered map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { GoogleChartsModule } from 'angular-google-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { GeomapComponent } from './geomap/geomap.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GeomapComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
