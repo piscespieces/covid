933bfd350d291aa99c39652f70acf361a0db774d
Andres Urdaneta <aeum3893@gmail.com>
2020-04-01 18:23:38 -0400

    new structure

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const scrapper = require('./scrapper/scrapper')
+const scrapper = require('./scrapper')
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
