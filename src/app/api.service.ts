105c92c181d07dc550b88029228c7768f407ccfb
Andres Urdaneta <aeum3893@gmail.com>
2020-04-02 15:17:09 -0400

    chaged port with env variables

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment'
+
+const API_URL = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080/api'
 
   constructor(private http: HttpClient) { }
 
   public getNews() {
-    return this.http.get(`${this.apiUrl}/countries`)
+    return this.http.get(API_URL + '/countries')
   }
 }
