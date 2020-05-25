a725160fad25957b9f2643cd35689f168e5cfd04
Andres Urdaneta <aeum3893@gmail.com>
2020-05-25 16:11:40 -0400

    fixed bug in incoming data that was displaying null instead of total cases, also updated the max number of cases in the geomap

--- src/app/home/geomap/geomap.component.ts
+++ src/app/home/geomap/geomap.component.ts
@@ -1,53 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../api.service';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../../api.service";
 
 @Component({
-  selector: 'app-geomap',
-  templateUrl: './geomap.component.html',
-  styleUrls: ['./geomap.component.scss']
+  selector: "app-geomap",
+  templateUrl: "./geomap.component.html",
+  styleUrls: ["./geomap.component.scss"],
 })
 export class GeomapComponent implements OnInit {
-
   countryJSON;
   data = [];
-  type = 'GeoChart'
-  columnNames = ["Country", "Cases"]
-  options = {}
+  type = "GeoChart";
+  columnNames = ["Country", "Cases"];
+  options = {};
 
-  constructor(private apiService: ApiService) {
-  }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getNews().subscribe((dataJSON) => {
-      this.countryJSON = dataJSON
+      this.countryJSON = dataJSON;
       for (let countryObject of this.countryJSON) {
         // Formatting "USA" name to "United States"
         if (countryObject.country_name === "USA") {
-          countryObject.country_name = "United States"
+          countryObject.country_name = "United States";
         }
         // If there is no deaths, instead of NaN, display the int 0
         if (countryObject.total_deaths === "") {
-          countryObject.total_deaths = 0
+          countryObject.total_deaths = 0;
         }
         //Pushing every object (Country) to a formatted array to be readable for the GeoMaps API
-        this.data.push(
-          [
-            countryObject.country_name,
-            parseInt(countryObject.total_cases),
-            parseInt(countryObject.total_deaths)
-          ])
+        this.data.push([
+          countryObject.country_name,
+          parseInt(countryObject.total_cases),
+          parseInt(countryObject.total_deaths),
+        ]);
       }
 
       // this.type = 'GeoChart'
-      this.columnNames = ["Country", "Cases", "Deaths"]
+      this.columnNames = ["Country", "Cases", "Deaths"];
       this.options = {
         colorAxis: {
-          values: [1, 100, 1000, 10000, 100000, 500000],
-          colors: ['#FFFFCC', '#FFCC00', '#FF9900', '#FF6600', 'FF3300', '#CC0000']
+          values: [10, 1000, 10000, 100000, 1000000, 2000000],
+          colors: [
+            "#FFFFCC",
+            "#FFCC00",
+            "#FF9900",
+            "#FF6600",
+            "FF3300",
+            "#CC0000",
+          ],
         },
-        legend: { textStyle: { italic: true } }
-      }
-
-    })
+        legend: { textStyle: { italic: true } },
+      };
+    });
   }
 }
